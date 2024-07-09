ee824708b1cd2d0ee7b633e26a13f8c6e5a90702
nguyenle1106 <153020288+nguyenle1106@users.noreply.github.com>
2024-07-09 12:38:50 +0700

    Initial Commit

---
+++ WeProfile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeProfile
+//
+//  Created by Nguyên Lê on 7/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
